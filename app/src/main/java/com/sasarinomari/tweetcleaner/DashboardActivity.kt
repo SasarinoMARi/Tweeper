b1dd8c066818f8d6f104f41197a7e34e4e04296b
Sasarino Mari <develop.mari@gmail.com>
2019-12-17 16:33:29 +0900

    AdMob 광고 추가!

--- app/src/main/java/com/sasarinomari/tweetcleaner/DashboardActivity.kt
+++ app/src/main/java/com/sasarinomari/tweetcleaner/DashboardActivity.kt
@@ -13,9 +13,14 @@ import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_dashboard.*
 import android.graphics.drawable.shapes.OvalShape
 import android.graphics.drawable.ShapeDrawable
-
-
-
+import android.widget.LinearLayout
+import androidx.core.view.size
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.initialization.InitializationStatus
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener
+import com.google.android.gms.ads.MobileAds
 
 class DashboardActivity : Adam() {
 
@@ -35,6 +40,22 @@ class DashboardActivity : Adam() {
 
         image_profilePicture.background = ShapeDrawable(OvalShape())
         image_profilePicture.clipToOutline = true
+
+        initAds()
+    }
+
+    private fun initAds() {
+        MobileAds.initialize(this)
+        val adView = AdView(this)
+        layout_ad.addView(adView)
+        adView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = if (BuildConfig.DEBUG) getString(R.string.banner_ad_unit_id_for_test)
+        else getString(R.string.banner_ad_unit_id)
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
     }
 
     private fun loadUserInformation() {

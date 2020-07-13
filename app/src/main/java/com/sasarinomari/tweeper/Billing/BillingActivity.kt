a81ea4d95b7c6d96d5e68c9725ed69ff8397478e
Sasarino MARi <develop.mari@gmail.com>
2020-07-13 23:55:47 +0900

    BillingProcess가 상품 목록을 불러와 그려주도록 작성

--- app/src/main/java/com/sasarinomari/tweeper/Billing/BillingActivity.kt
+++ app/src/main/java/com/sasarinomari/tweeper/Billing/BillingActivity.kt
@@ -1,16 +1,23 @@
 package com.sasarinomari.tweeper.Billing
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.anjlab.android.iab.v3.BillingProcessor
+import com.anjlab.android.iab.v3.SkuDetails
 import com.anjlab.android.iab.v3.TransactionDetails
 import com.sasarinomari.tweeper.Base.BaseActivity
 import com.sasarinomari.tweeper.R
 import com.sasarinomari.tweeper.RecyclerInjector
 import kotlinx.android.synthetic.main.fragment_title_with_desc.view.*
 import kotlinx.android.synthetic.main.full_recycler_view.*
+import kotlinx.android.synthetic.main.item_default.view.*
+import kotlinx.android.synthetic.main.item_sku.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 open class BillingActivity : BaseActivity(), BillingProcessor.IBillingHandler {
     private lateinit var bp: BillingProcessor
@@ -29,19 +36,56 @@ open class BillingActivity : BaseActivity(), BillingProcessor.IBillingHandler {
     override fun onBillingInitialized() {
         setContentView(R.layout.full_recycler_view)
 
+
+        val purchaseListingDetails: ArrayList<SkuDetails> = ArrayList(bp.getPurchaseListingDetails(arrayListOf(
+            DonationItems.donate1000.name,
+            DonationItems.donate2000.name,
+            DonationItems.donate5000.name,
+            DonationItems.donate8000.name,
+            DonationItems.donate13000.name
+        )).sortedWith(compareBy { it.priceLong }))
+
+
         val adapter = RecyclerInjector()
         adapter.add(object: RecyclerInjector.RecyclerFragment(R.layout.fragment_title_with_desc) {
             override fun draw(view: View, item: Any?, viewType: Int, listItemIndex: Int) {
                 view.title_text.text = getString(R.string.Donate)
                 view.title_description.text = getString(R.string.DonateDesc)
 
-                view.setOnClickListener {
-                    bp.purchase(this@BillingActivity, DonationItems.donate1000.name)
-                }
             }
        })
+
+        adapter.add(object: RecyclerInjector.RecyclerFragment(R.layout.item_sku, purchaseListingDetails) {
+            @SuppressLint("SetTextI18n", "SimpleDateFormat")
+            override fun draw(view: View, item: Any?, viewType: Int, listItemIndex: Int) {
+                item as SkuDetails
+                view.skuitem_title.text = item.title.removeSuffix(getString(R.string.skuPostfix))
+                view.skuitem_description.text = item.description
+                view.skuitem_price.text = item.priceText
+
+                view.setOnClickListener {
+                    bp.purchase(this@BillingActivity, item.productId)
+                }
+            }
+        })
+        adapter.add(object: RecyclerInjector.RecyclerFragment(R.layout.item_default) {
+            @SuppressLint("SetTextI18n", "SimpleDateFormat")
+            override fun draw(view: View, item: Any?, viewType: Int, listItemIndex: Int) {
+                view.defaultitem_title.text = "구매 내역 복원하기"
+                view.defaultitem_description.text = "기존 구매 이력을 불러와요. 추가 과금은 되지 않아요!"
+
+                view.setOnClickListener {
+                    restore()
+                }
+            }
+        })
+
         root.layoutManager = LinearLayoutManager(this)
         root.adapter = adapter
+    }
+
+    private fun restore() {
+        TODO("Not yet implemented")
     }
 
     override fun onPurchaseHistoryRestored() {
